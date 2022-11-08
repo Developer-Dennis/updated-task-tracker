59372a8bf69a878c1f642b2d45d632841d97873f
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-08 12:42:54 +0300

    Task Tracker application

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="onClick()" :style="
+    {background:color}" class="btn">{{text}}
+    </button>
+</template>
+
+
+<script>
+    export default {
+        name: 'addButton',
+        props:{
+            text:String,
+            color:String,
+        },
+        methods:{
+            onClick(){
+                console.log('click')
+            },
+        }
+    }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1> {{title}} </h1>
+        <addButton text="Add Task" color="green" />
+    </header>
+</template>
+
+
+
+<script>
+import addButton from './Button.vue'
+export default {
+    name:'head_der',
+    props:{
+        title:String
+    },
+    components: {
+        addButton,
+}
+}
+
+</script>
+
+ 
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
